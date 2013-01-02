2924b3abe2993193e5d7a54f43de6aab0d4d2125
Rajan Punchouty <rajan@ezzie.in>
2013-01-02 12:36:36 +0530

    Initial Commit - Project  Creation and some changes to .gitignore and Gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MainApp::Application.config.secret_token = '0ef2c7d6533b5fd173b8d849a26357fc501b866fad438c107873ce970ef8f386cd69b509c9d347cefe5ed15fad1f4abe9133e9a79e9a4e04ae4e766ff83f7d4f'
